e3760c67603132949d2a98a2d25ebe73cd5243f9
shivamGandhi <shivamgandhi95@gmail.com>
2018-12-07 08:59:26 -0500

    Dao class created

---
+++ app/src/main/java/com/example/android/todo_list/Screens/Dao/NoteDao.java
@@ -0,0 +1,31 @@
+package com.example.android.todo_list.Screens.Dao;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import com.example.android.todo_list.Screens.Entity.Note;
+
+import java.util.List;
+
+@Dao
+public interface NoteDao {
+
+    @Insert
+    void insert(Note note);
+
+    @Delete
+    void delete(Note note);
+
+    @Update
+    void update(Note note);
+
+    @Query("DELETE FROM note_table")
+    void deleteAll();
+
+    @Query("SELECT * FROM note_table ORDER BY priority DESC")
+    LiveData<List<Note>> getAllNotes();
+}
