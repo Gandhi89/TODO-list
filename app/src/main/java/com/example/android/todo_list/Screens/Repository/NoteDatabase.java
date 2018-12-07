132452e28417a06e980e39704e7cd7067cffaebe
shivamGandhi <shivamgandhi95@gmail.com>
2018-12-07 09:11:24 -0500

    Database created

---
+++ app/src/main/java/com/example/android/todo_list/Screens/Repository/NoteDatabase.java
@@ -0,0 +1,26 @@
+package com.example.android.todo_list.Screens.Repository;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+
+import com.example.android.todo_list.Screens.Dao.NoteDao;
+import com.example.android.todo_list.Screens.Entity.Note;
+
+@Database(entities = Note.class,version = 1)
+public abstract class NoteDatabase extends RoomDatabase {
+
+    public abstract NoteDao noteDao();
+
+    private static NoteDatabase instance;
+
+    static NoteDatabase getInstance(Context context){
+        if (instance == null){
+            instance = Room.databaseBuilder(context,NoteDatabase.class,"note_database")
+                    .build();
+        }
+        return instance;
+    }
+
+}
